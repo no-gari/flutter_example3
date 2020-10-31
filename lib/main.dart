6a2ca6bcca396e226043046f4d2bac3355c66616
nohjonghyeok <sofaissofa@icloud.com>
2020-10-31 19:30:56 +0900

    learn mainAxixAlignment, crossAxisAlignent

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('container1'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.blueGrey[900],
+                child: Text('container1'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.red[700],
+                child: Text('container1'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
