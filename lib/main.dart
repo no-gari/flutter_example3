310b807ce7011ee458dc2aba6bf6fa396f6bf920
nohjonghyeok <sofaissofa@icloud.com>
2020-11-03 23:14:45 +0900

    flutter card using containers only

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,51 @@ class MyApp extends StatelessWidget{
                       fontWeight: FontWeight.bold,
                       color: Colors.teal[100],
                     ),
-                  )
+                  ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children:<Widget> [
+                      Icon(Icons.phone,
+                      color: Colors.teal,),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '010-1234-1234',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontSize: 20.0,
+                          fontFamily: 'SourceSansPro',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children:<Widget> [
+                      Icon(Icons.email,
+                        color: Colors.teal,),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'hihi@hihi.com',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontSize: 20.0,
+                          fontFamily: 'SourceSansPro',
+                        ),
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
