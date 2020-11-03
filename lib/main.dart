79faf90e41f036fec93d18293e13eda7d34edad9
nohjonghyeok <sofaissofa@icloud.com>
2020-11-03 22:43:09 +0900

    added fonts and texts

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,35 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.blue,
-                backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQEclOY26bFguA_kb5E6qBN3eVpYX8TQrqc2A&usqp=CAU'),
-              ),
-                Text(
-                  '저커버그ㅋㅋ',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.blue,
+                  backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQEclOY26bFguA_kb5E6qBN3eVpYX8TQrqc2A&usqp=CAU'),
+                ),
+                  Text(
+                    'michael noh',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: 'Pacifico',
+                    ),
                   ),
-                )
-            ],
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal[100],
+                    ),
+                  )
+              ],
+            ),
           ),
         ),
       ),
